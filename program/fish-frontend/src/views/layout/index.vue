7d80af2aa0ea0cd9a746c4d06347bfbf9f2d8d0f
xiaoyangii <90941964+xiaoyangii@users.noreply.github.com>
2023-11-10 20:34:04 +0800

    [zkq]feat: Initialize the project
    Sliced modules, built routes, and written login interface

---
+++ program/fish-frontend/src/views/layout/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="layout">
+    <router-view></router-view>
+    index
+    <button @click="$router.push('/rank')">rank</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'layoutIndex',
+  components: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  created () {},
+}
+</script>
+<style scoped lang='less'>
+
+</style>
