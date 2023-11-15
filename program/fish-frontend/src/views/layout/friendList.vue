6af6e455500099db6fa852c835bdd4f8e2366c95
xiaoyangii <90941964+xiaoyangii@users.noreply.github.com>
2023-11-15 18:37:39 +0800

    [zkq] feat: add friendList page

--- program/fish-frontend/src/views/layout/friendList.vue
+++ program/fish-frontend/src/views/layout/friendList.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="fri">
-    fri
+    <div class="fri_wrap">
+      <Fri :style="customStyle"></Fri>
+      <backBtn />
+    </div>
   </div>
 </template>
 
 <script>
+import Fri from '@/components/fri.vue'
+import backBtn from '@/components/backBtn.vue'
 export default {
   name: 'friendList',
-  components: {},
+  components: {
+    Fri,
+    backBtn
+  },
   data () {
-    return {}
+    return {
+      customStyle: {
+        height: '70vh',
+        width: '25vw'
+      }
+    }
   },
   computed: {},
   watch: {},
@@ -18,5 +31,20 @@ export default {
 }
 </script>
 <style scoped lang='less'>
-/* @import url(); 引入公共css类 */
+.fri {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  &_wrap {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 50vw;
+    height: 90vh;
+  }
+}
 </style>
